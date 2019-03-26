658cb65018e6a5080f7e2096fba4bcd0672b01e7
davemeneses <meneses.coffee@gmail.com>
2019-03-26 18:03:06 -0500

    installed all required packages

--- liri.js
+++ liri.js
@@ -1,64 +1,57 @@
-var keys = require("./keys.js")
+var keys = require("./keys.js");
 var axios = require("axios");
 var userInput = "";
 userInput = process.argv;
-var command = process.argv[2]
+var command = process.argv[2];
 var printed = "";
 var searchTerm = "";
 // var Spotify = require("node-spotify-api");
 // var spotify = new Spotify({
 //     id: ,
-//     secret: 
+//     secret:
 //   });
 
 for (var i = 3; i < userInput.length; i++) {
-    printed = printed + " " + userInput[i];
+  printed = printed + " " + userInput[i];
 }
 
 for (var i = 3; i < userInput.length; i++) {
-    if (i > 3 && i < userInput.length) {
-        searchTerm = searchTerm + "+" + userInput[i];
-    }
-    else {
-        searchTerm += userInput[i];
-    }
+  if (i > 3 && i < userInput.length) {
+    searchTerm = searchTerm + "+" + userInput[i];
+  } else {
+    searchTerm += userInput[i];
+  }
 }
 
 if (command == "movie-this" && userInput) {
-    console.log("Your movie is:" + printed);
-    
-    axios
+  console.log("Your movie is:" + printed);
+
+  axios
     .get(
-        "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy"
+      "http://www.omdbapi.com/?t=" +
+        searchTerm +
+        "&y=&plot=short&apikey=trilogy"
     )
     .then(function(response) {
       console.log("The movie's rating is: " + response.data.imdbRating);
     });
-
-
 } else if (command == "concert-this" && userInput) {
-    console.log("Your concert is:" + printed);
-    
-    axios
+  console.log("Your concert is:" + printed);
+
+  axios
     .get(
-        "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy"
+      "http://www.omdbapi.com/?t=" +
+        searchTerm +
+        "&y=&plot=short&apikey=trilogy"
     )
     .then(function(response) {
       console.log("The movie's rating is: " + response.data.imdbRating);
     });
-
 } else if (command == "spotify-this-song" && userInput) {
-    console.log("Your song is:" + printed);
-
+  console.log("Your song is:" + printed);
 } else if (command == "do-what-it-says" && userInput) {
-    console.log("BITCH:" + printed);
+  console.log("BITCH:" + printed);
 }
-
-
-
-
-
-
 
 // function getMovie() {
 
