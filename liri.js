a7f886aa9a78689c9914d49c357f6e57b3a9390c
davemeneses <meneses.coffee@gmail.com>
2019-03-26 22:49:40 -0500

    finished the homework with my tutors help

--- liri.js
+++ liri.js
@@ -1,20 +1,25 @@
-var keys = require("./keys.js");
+require("dotenv").config();
+var fs = require("fs");
+var keys = require("./keys");
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
 var axios = require("axios");
+var spotify = new Spotify(keys.spotify);
 var userInput = "";
 userInput = process.argv;
 var command = process.argv[2];
 var printed = "";
 var searchTerm = "";
-// var Spotify = require("node-spotify-api");
-// var spotify = new Spotify({
-//     id: ,
-//     secret:
-//   });
+
+console.log(
+  "\nACCEPTED COMMANDS: movie-this, concert-this, spotify-this-song, do-what-it-says\n"
+);
 
 for (var i = 3; i < userInput.length; i++) {
   printed = printed + " " + userInput[i];
 }
 
+// Creating the search term
 for (var i = 3; i < userInput.length; i++) {
   if (i > 3 && i < userInput.length) {
     searchTerm = searchTerm + "+" + userInput[i];
@@ -24,42 +29,97 @@ for (var i = 3; i < userInput.length; i++) {
 }
 
 if (command == "movie-this" && userInput) {
-  console.log("Your movie is:" + printed);
-
-  axios
-    .get(
-      "http://www.omdbapi.com/?t=" +
-        searchTerm +
-        "&y=&plot=short&apikey=trilogy"
-    )
-    .then(function(response) {
-      console.log("The movie's rating is: " + response.data.imdbRating);
-    });
+  console.log("MOVIE: " + printed + "\n");
+  movieThis();
 } else if (command == "concert-this" && userInput) {
-  console.log("Your concert is:" + printed);
-
-  axios
-    .get(
-      "http://www.omdbapi.com/?t=" +
-        searchTerm +
-        "&y=&plot=short&apikey=trilogy"
-    )
-    .then(function(response) {
-      console.log("The movie's rating is: " + response.data.imdbRating);
-    });
+  console.log("ARTIST: " + printed + "\n");
+  concertThis();
 } else if (command == "spotify-this-song" && userInput) {
-  console.log("Your song is:" + printed);
+  spotifyThis();
 } else if (command == "do-what-it-says" && userInput) {
-  console.log("BITCH:" + printed);
+  doWhatItSays();
 }
 
-// function getMovie() {
+function movieThis() {
+  axios
+    .get(
+      "http://www.omdbapi.com/?t=" +
+        searchTerm +
+        "&y=&plot=short&apikey=trilogy"
+    )
+    .then(function(response) {
+      console.log(
+        "\nTitle: " +
+          response.data.Title +
+          "\nRelease Year: " +
+          response.data.Year +
+          "\nIMDB Rating: " +
+          response.data.imdbRating +
+          "\nRotten Tomatoes Rating: " +
+          response.data.Ratings[1].Value +
+          "\nLocation: " +
+          response.data.Country +
+          "\nLanguage: " +
+          response.data.Language +
+          "\nPlot: " +
+          response.data.Plot +
+          "\nCast: " +
+          response.data.Actors
+      );
+    });
+}
 
-//     axios
-//         .get(
-//             "http://www.omdbapi.com/?t=" + inputMovie + "&y=&plot=short&apikey=trilogy"
-//         )
-//         .then(function (response) {
-//             console.log("The movie's rating is: " + response.data.imdbRating);
-//         });
-// }
+function concertThis() {
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        searchTerm +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      for (i = 0; i < 3; i++)
+        console.log(
+          "VENUE: " +
+            response.data[i].venue.name +
+            "\nLOCATION: " +
+            response.data[i].venue.city +
+            "\nDATE: " +
+            moment(response.data[i].datetime).format("MM/DD/YYYY") +
+            "\n"
+        );
+    });
+}
+
+function spotifyThis() {
+  // searchTerm.toString
+  spotify.search({ type: "track", query: searchTerm }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(
+      "ARTIST: " +
+        data.tracks.items[0].artists[0].name +
+        "\nSONG: " +
+        data.tracks.items[0].name +
+        "\nALBUM: " +
+        data.tracks.items[0].album.name +
+        "\nPREVIEW: " +
+        data.tracks.items[0].preview_url +
+        "\n"
+    );
+  });
+}
+
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      console.log("Error! " + err);
+    }
+
+    var randomThing = data.split(",");
+    searchTerm = randomThing[1];
+    console.log("THE UNIVERSE GRANTS YOU -- \n");
+    spotifyThis();
+  });
+}
